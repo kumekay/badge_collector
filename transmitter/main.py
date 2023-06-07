058a1ac58d3e024ef326c7ac13279b0ec2c1be7f
Sergei Silnov <sergei.silnov@espressif.com>
2023-06-07 14:26:57 +0200

    Send to peer only

--- transmitter/main.py
+++ transmitter/main.py
@@ -3,14 +3,14 @@ import json
 
 import espnow
 
-BROADCAST_PEER = espnow.Peer(b"\xff" * 6)
+# BROADCAST_PEER = espnow.Peer(b"\xff" * 6)
 
 # Dictionary of known peers
 # Keys are MAC addresses, values - number of packets sent
 known_peers = {}
 
 # Question to be sent
-# Format: (question, (answers), correct_answer_index)
+# Format: (question, (answers), correct_answer_index, secret_char, secret_char_position)
 QUESTION = (
     "The first ESP chip is ...",
     (
@@ -20,6 +20,8 @@ QUESTION = (
         "ESP32-S2",
     ),
     1,
+    "E",
+    2,
 )
 QUESTION_BYTES = json.dumps(QUESTION).encode("utf-8")
 
@@ -33,6 +35,7 @@ async def listener(enow):
             # If we don't know this peer, add it to the dictionary
             if packet.msg == b"makers!" and packet.mac not in known_peers:
                 print("New peer found:", packet.mac)
+                enow.peers.append(espnow.Peer(packet.mac))
                 known_peers[packet.mac] = 0
 
         await asyncio.sleep(0)
@@ -41,13 +44,14 @@ async def listener(enow):
 async def sender(enow):
     while True:
         # Send question to all known peers 5 times, then remove them from the list
-        for peer in known_peers:
-            if known_peers[peer] < 5:
+        for peer in enow.peers:
+            if known_peers[peer.mac] < 5:
                 print("Sending question")
-                enow.send(QUESTION_BYTES, BROADCAST_PEER)
-                known_peers[peer] += 1
+                enow.send(QUESTION_BYTES, peer)
+                known_peers[peer.mac] += 1
             else:
-                del known_peers[peer]
+                enow.peers.remove(peer)
+                del known_peers[peer.mac]
 
         await asyncio.sleep(2)
 
@@ -55,7 +59,6 @@ async def sender(enow):
 async def main():
     # Init espnow with broadcast
     enow = espnow.ESPNow()
-    enow.peers.append(BROADCAST_PEER)
 
     listener_task = asyncio.create_task(listener(enow))
     sender_task = asyncio.create_task(sender(enow))
